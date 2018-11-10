cc218be50826a734a54a6b63ea04bc7598447e45
tangmore <tangmore@163.com>
2018-11-10 13:34:39 +0800

    new file

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+// var tabs=document.querySelectorAll('.destination [data-toggle=tab]');
+// for(var tab of tabs){
+//     tab.onclick=function(){
+//         var tab=this;
+//         var id=tab.getAttribute('data-id');
+//         var div=document.querySelector(id);
+
+//         var divs=document.querySelectorAll('#tab>div');
+//         //先将所有的清零
+//         for(var div of divs){
+//             div.style.zIndex='';
+//         }
+//         div.style.zIndex=99;
+//     }
+// }
