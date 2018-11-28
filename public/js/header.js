923e7b2569beaa99544106b617169893a3bbbd47
wlm <276374436@qq.com>
2018-11-28 20:26:46 +0800

    header添加新功能

--- public/js/header.js
+++ public/js/header.js
@@ -7,28 +7,43 @@ $(function(){
         type: "get",
         success: function (res) {
             $(res).replaceAll("header");
+            function navSpreed(elem){
+                $(elem).children(":first").css("animation","topChange0 0.5s linear forwards")
+                        .next().css("animation","bottomChange0 0.5s linear forwards");
+                    $(elem).attr("data-status", 1);
+                    //下拉选项框变化
+                    $("[data-target=collapse_navbar]").removeClass("collapse-navbar-0").addClass("collapse-navbar-1");
+            }
+            function navFold(elem){
+                $(elem).children(":first").css("animation", "topChange1 0.5s linear forwards")
+                        .next().css("animation","bottomChange1 0.5s linear forwards");
+                    $(elem).attr("data-status", 0);
+                    //下拉选项框变化
+                    $("[data-target=collapse_navbar]").removeClass("collapse-navbar-1").addClass("collapse-navbar-0");
+            }
             //region header-nav事件
             //导航按钮事件
-            $("[data-trigger=navIcon]").click(function() {
+            $("[data-trigger=navIcon]").click(function (){
                 var $dataStatus = $(this).attr("data-status");
                 if($dataStatus == 0){
                     //导航按钮变化
-                    $(this).children(":first").css("animation","topChange0 0.5s linear forwards")
-                        .next().css("animation","bottomChange0 0.5s linear forwards");
-                    $(this).attr("data-status", 1);
-                    //下拉选项框变化
-                    $("[data-target=collapse_navbar]").removeClass("collapse-navbar-0").addClass("collapse-navbar-1");
+                    navSpreed(this);
                     return;
                 }
                 if($dataStatus == 1){
-                    $(this).children(":first").css("animation", "topChange1 0.5s linear forwards")
-                        .next().css("animation","bottomChange1 0.5s linear forwards");
-                    $(this).attr("data-status", 0);
-                    //下拉选项框变化
-                    $("[data-target=collapse_navbar]").removeClass("collapse-navbar-1").addClass("collapse-navbar-0");
+                    navFold(this);
                     return;
                 }
-            })
+            });
+            // 个人中心事件
+            $("[data-trigger=toggle]").on("mouseover",function(){
+                var $dataStatus = $("[data-trigger=navIcon]").attr("data-status");
+                if($dataStatus == 1){
+                    navFold("[data-trigger=navIcon]");
+                }
+                $("[data-target=toggleItem]").removeClass("d-none").addClass("d-flex");
+            }).on("mouseout",function(){
+                $("[data-target=toggleItem]").removeClass("d-flex").addClass("d-none")})
             //搜索icon事件
             $("[data-trigger=searchIcon]").click( function(){
                 //弹出搜索栏
