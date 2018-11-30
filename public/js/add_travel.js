9d046c2e3b9b7632cff58e4dd2f4a1b8e91d4ca4
wlm <276374436@qq.com>
2018-11-30 20:29:44 +0800

    add_travel添加新功能

--- public/js/add_travel.js
+++ public/js/add_travel.js
@@ -2,4 +2,10 @@ $(function(){
     $("#arrow").click(function(){
         $(this).toggleClass("icon-arrow_l").toggleClass("icon-arrow-r").parent().next().toggleClass("spread");
     })
+
+    $("#tarea").scroll(function(){
+        this.rows++;
+        this.offsetX>0;
+    })
+    
   })
