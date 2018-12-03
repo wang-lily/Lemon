ee0473d4629ebad6e3825b6f917c3c4097e05248
tangmore <tangmore@163.com>
2018-12-03 20:33:23 +0800

    完善表内关键字

--- lemon.sql
+++ lemon.sql
@@ -8,53 +8,67 @@ CREATE TABLE user(
 	uname VARCHAR(16),
 	upwd VARCHAR(16),
 	phone CHAR(11),
+	sex INT,
 	email VARCHAR(32),
 	#游记表id
-	vid INT	
+	vid INT,
+	profile VARCHAR(125)
 );
 #轮播表
 CREATE TABLE carousel(
 	cid INT PRIMARY KEY AUTO_INCREMENT,
-	cimg_path VARCHAR(32)
+	cimg_path VARCHAR(32),
+	spot VARCHAR(50)
 );
-#地点表
-CREATE TABLE address(
-	aid INT PRIMARY KEY AUTO_INCREMENT,
-	address VARCHAR(32)
-);
+
 #景点表
 CREATE TABLE spot(
 	sid INT PRIMARY KEY AUTO_INCREMENT,
-	aid INT
+	country VARCHAR(48),
+	spot VARCHAR(48),
+	iid  INT
 );
+
 #图片总表
 CREATE TABLE img(
 	iid INT PRIMARY KEY AUTO_INCREMENT,
-	#图片所属的景点id
-	sid INT,
-	#图片所属的地点id
-	aid INT
+	iimg_path VARCHAR(50)
 );
 #游记表
 CREATE TABLE travel(
 	tid INT PRIMARY KEY AUTO_INCREMENT,
-	content TEXT,
+	txt TEXT,
 	#用户id
 	uid INT,
 	#图片id
 	iid INT,
 	Ttime DATETIME,
 	#浏览量
-	tview INT
+	tview INT,
+	#点赞
+	zan INT,
+	btitle VARCHAR(50),
+	mtitle VARCHAR(50),
+	stitle VARCHAR(50),
+	comment VARCHAR(200),
+	video_path VARCHAR(60),
 );
-#指南表
+#攻略表
 CREATE TABLE pointer(
 	pid INT PRIMARY KEY AUTO_INCREMENT,
 	#景点id
 	sid INT,
 	#图片id
 	iid INT,
-	content TEXT,
+	txt TEXT,
 	#浏览量
-	pview INT
+	pview INT,
+	Ttime DATETIME,
+	#浏览量
+	tview INT,
+	#点赞
+	zan INT,
+	btitle VARCHAR(50),
+	mtitle VARCHAR(50),
+	stitle VARCHAR(50),
 );
