e4c488429bb402e0b45d3572607608580a20c642
wang-lily <276374435@qq.com>
2018-12-13 00:30:29 +0800

    完善轮播

--- lemon_vue/src/views/Spots.vue
+++ lemon_vue/src/views/Spots.vue
@@ -1,17 +1,16 @@
 <template>
     <section class="mb-5">
         <!--轮播start-->
-    <div id="header" class="container-fluid position-relative p-0" >
+    <div id="header" class="container-fluid position-relative p-0" @mouseover="pauseCarousel" @mouseout="startCarousel">
         <div class="w-100 position-relative">
             <picture>
-                <source media="(min-width: 1000px)" :srcset="carouselImgs[1].lg_url">
-                <source media="(max-width:999px)" :srcset="carouselImgs[1].md_url">
-                <img class="w-100" :src="carouselImgs[1].src">
+                <source media="(min-width: 1000px)" :srcset="carousel.Imgs[1].lg_url">
+                <source media="(max-width:999px)" :srcset="carousel.Imgs[1].md_url">
+                <img class="w-100" :src="carousel.Imgs[1].src">
             </picture>
-            <!--<img class="position-relative w-100" src="../img/carousel/lb01.png">-->
         </div>
         <div class="w-100 position-absolute" data-trigger="lunbo">
-            <div  v-for="img in carouselImgs" :key="img.cid"   class="position-absolute w-100" :class="img.cid==carouselCid?'in':'out'"  data-target="lunboItem">
+            <div  v-for="img in carousel.Imgs" :key="img.cid"   class="position-absolute w-100" :class="img.cid==carousel.cid?'in':'out'"  data-target="lunboItem">
                 <picture>
                     <source media="(min-width: 1000px)" :srcset="img.lg_url">
                     <source media="(max-width:999px)" :srcset="img.md_url">
@@ -24,30 +23,6 @@
                     <a>{{img.spot}}</a>
                 </p>
             </div>
-            <!-- <div  class="position-absolute w-100 out" data-target="lunboItem">
-                <picture>
-                    <source media="(min-width: 1000px)" srcset="../img/carousel/lb16.png">
-                    <source media="(max-width:999px)" srcset="../img/carousel/lb16_500x250.png">
-                    <img class="w-100" src="../img/carousel/picture.png">
-                </picture>
-                <p class="w-100 pr-5 pl-5 mb-1 text-center">
-                    <span class="iconfont icon-tubiao"></span>&nbsp;&gt;
-                    <a>中国</a>&nbsp;&gt;
-                    <a>西藏</a>
-                </p>
-            </div> -->
-            <!-- <div  class="position-absolute w-100 out" data-target="lunboItem">
-                <picture>
-                    <source media="(min-width: 1000px)" srcset="../img/carousel/lb11.png">
-                    <source media="(max-width:999px)" srcset="../img/carousel/lb11_500x250.png">
-                    <img class="w-100" src="../img/carousel/picture.png">
-                </picture>
-                <p class="w-100 pr-5 pl-5 mb-1 text-center">
-                    <span class="iconfont icon-tubiao"></span>&nbsp;&gt;
-                    <a>中国</a>&nbsp;&gt;
-                    <a>苏州</a>
-                </p>
-            </div> -->
         </div>
         <div class="w-100 d-none d-sm-block search-input">
             <p class="w-100 text-center text-white">
@@ -68,35 +43,45 @@
         data(){
             return {
                 background:"background: url(`../img/tab/tab01.png`) no-repeat center center;",
-                carouselImgs:["","","",""],
-                carouselCid:14
+                // -------------------------------轮播数据start---------------------------------------------------
+                carousel:{
+                    timer:null,
+                    Imgs:["","","",""],
+                    cid:14,
+                    i:0
+                }
+                // --------------------------------轮播数据start--------------------------------------------
             }
         },
         methods:{
+            // -------------------------------------轮播方法start------------------------------------------------
             loadCarousel(){
                 this.axios.get(`http://127.0.0.1:3001/spots/carousel`,{params:
                     {num1:9, num2:11, num3:14, num4:16}}).then(res=>{
-                        this.carouselImgs = res.data;
-                        console.log(res.data);
+                        this.carousel.Imgs = res.data;
+                        // console.log(res.data);
                     })
             },
-            carousel(){
-                var i = 0;
-                function task(){
-                    if(i==this.carouselImgs.length){
-                        i=0;
-                    }
-                    this.carouselCid = this.carouselImgs[i].cid;
-                    i++;
-                }
-                var timer = setInterval(task.bind(this),3000);
+            task(){
+                var index = this.carousel.i%this.carousel.Imgs.length;
+                console.log(index);
+                this.carousel.cid = this.carousel.Imgs[index].cid;
+                this.carousel.i++;
+                },
+            startCarousel(){
+                this.timer = setInterval(this.task,3000);
+            },
+            pauseCarousel(){
+                clearInterval(this.timer);
             }
+            // -----------------------------------轮播方法end--------------------------------------------------
         },
         created() {
             this.loadCarousel();
+            this.startCarousel();
         },
         mounted() {
-            this.carousel();
+            
         }
     }
 </script>
